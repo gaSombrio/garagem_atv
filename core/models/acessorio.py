b76cf09bc0e57e849734b9567784fe4e63035a35
gaSombrio <sombrio.ga@gmail.com>
2026-03-24 14:05:28 -0300

    feat: adição das models, views e serializers da cor

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Acessorio(models.Model):
-    descricao = models.TextField(blank=True, null=True)
+    descricao = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.descricao
